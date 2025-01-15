01dcba1a0c5c3dbfe7173f4f10d9cf5c731555d6
joafern2 <joaofsaguas@gmail.com>
2025-01-15 22:59:53 +0000

    added execve and updated script. malloc problems

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 if [[ $1 == "-e" ]]; then
 	cc -Werror -Wextra -Wall srcs/exec.c srcs/error.c libft/*.c && ./a.out
+elif [[ $1 == "-ve" ]]; then
+	cc -Werror -Wextra -Wall srcs/exec.c srcs/error.c libft/*.c && valgrind ./a.out
 else
 	echo "not set"
 fi
--- srcs/exec.c
+++ srcs/exec.c
@@ -6,11 +6,13 @@
 /*   By: joafern2 <joafern2@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 22:12:08 by joafern2          #+#    #+#             */
-/*   Updated: 2025/01/15 04:40:23 by joafern2         ###   ########.fr       */
+/*   Updated: 2025/01/15 22:38:10 by joafern2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
+
+extern char **environ; // real environment
 
 int	is_builtin(char **arg)
 {
@@ -18,11 +20,41 @@ int	is_builtin(char **arg)
 	return (0);	
 }
 
+char	*find_path(char *cmd)
+{
+	char	*path_env = getenv("PATH"); // using real env
+	char	**path_dir;
+	char	*full_path;
+	char	*temp;
+	int	i;
+
+	i = 0;
+	if (!path_env)
+		return (NULL);
+	path_dir = ft_split(path_env, ':');
+	if (!path_dir)
+		deallocate("Failed to split $PATH\n");
+	while (path_dir[i])
+	{
+		temp = ft_strjoin(path_dir[i], "/");
+		full_path = ft_strjoin(temp, cmd);
+		if (!full_path)
+			deallocate("Failed to join command to $PATH\n");
+		if (access(full_path, X_OK) == 0)
+			return (full_path);
+		free(temp);
+		free(full_path);
+		i++;
+	}
+	return (NULL);
+}
+
 void	exec_cmd(t_ms *ms)
 {
 	int	i;
 	char	*cmd;
 	char	**arg;
+	char	*path;
 
 	i = 0;
 	while (ms->cmd[i])
@@ -36,9 +68,15 @@ void	exec_cmd(t_ms *ms)
 		}
 		else if (fork() == 0)
 		{
-			// execve not in use
-			if (execvp(cmd, arg) == -1)
-				deallocate("Error executing execve.\n"); 
+			path = find_path(cmd);
+			if (!path)
+				deallocate("$PATH for command not found\n"); 
+			if (execve(path, arg, environ) == -1) // real env in use
+			{
+				deallocate("Error executing execve.\n");
+				free(path);
+			}
+			free(path);
 		}
 		else
 			wait(NULL);
