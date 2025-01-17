aff67007b28e73555f6abc532e5df1f7e29da810
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-01-17 18:41:36 +0000

    create and assign env copy to main struct

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,29 +6,31 @@
 /*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 19:22:10 by rafasant          #+#    #+#             */
-/*   Updated: 2025/01/15 00:34:47 by joafern2         ###   ########.fr       */
+/*   Updated: 2025/01/15 21:51:07 by rafasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
 // echo 123 | cat -e | wc -l
-/*
-int	main(int argc, char **argv)
+
+int	main(int argc, char **argv, char **env)
 {
 	char	*input;
+	t_ms	*ms;
 
 	(void)argv;
 	if (argc != 1)
 		return (ft_printf("Error: Invalid number of arguments.\n"));
+	ms = init_ms(env);
 	while (1)
 	{
 		input = readline("$minishell>");
-		parse_input(input);
+		parse_input(ms, input);
 		ft_printf("input>%s\n", input);
 		add_history(input);
 		//execute();
 		if (!ft_strncmp(input, "exit", 4))
 			break ;
 	}
-}*/
+}
