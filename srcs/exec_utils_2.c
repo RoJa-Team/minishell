4886cf53308013b8274ec952bbb155984366f133
joao <joaofsaguas@gmail.com>
2025-04-22 18:48:40 +0100

    more norminette

---
+++ srcs/exec_utils_2.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_utils_2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joafern2 <joafern2@student.42lisboa.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 18:20:25 by joafern2          #+#    #+#             */
+/*   Updated: 2025/04/22 18:25:39 by joafern2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minishell.h"
+
+void	child_process(int prev_fd, int *fd, int i)
+{
+	if (ms()->cmd[i]->fd_in || ms()->cmd[i]->fd_out)
+	{
+		if (handle_redirections(ms()->cmd[i]) != 0)
+			return ;
+	}
+	if (prev_fd != -1 && ms()->cmd[i]->fd_in == NULL)
+		dup2(prev_fd, STDIN_FILENO);
+	if (prev_fd != -1)
+		close(prev_fd);
+	if (ms()->cmd[i + 1] && ms()->cmd[i]->fd_out == NULL)
+		dup2(fd[1], STDOUT_FILENO);
+	if (ms()->cmd[i + 1])
+	{
+		close(fd[1]);
+		close(fd[0]);
+	}
+	if (is_builtin(i))
+	{
+		execute_builtin(i);
+		clean_structs();
+		exit (0);
+	}
+	else
+		execute_execve(i);
+}
+
+void	invoke_shell(int i, char *path)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		if (execve(path, ms()->cmd[i]->arg, ms()->ms_env) == -1)
+			deallocate("Error executing execve: execute_execve\n");
+	}
+	else
+		waitpid(pid, NULL, 0);
+}
+
+void	not_found(int i)
+{
+	write(2, ms()->cmd[i]->arg[0], ft_strlen(ms()->cmd[i]->arg[0]));
+	write(2, ": command not found\n", 20);
+	ms()->exit_status = 127;
+	clean_structs();
+	exit(127);
+}
+
+void	execute_execve(int i)
+{
+	char				*path;
+	struct sigaction	sa;
+
+	path = NULL;
+	sa.sa_handler = SIG_DFL;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	if ((ms()->cmd[i]->arg[0][0] != '/')
+		&& (ft_strncmp((ms()->cmd[i])->arg[0], "./", 2) != 0))
+		path = find_path(ms()->env_lst, ms()->cmd[i]->arg[0]);
+	else
+	{
+		if (access(ms()->cmd[i]->arg[0], X_OK) == 0)
+		{
+			path = ms()->cmd[i]->arg[0];
+			if ((ft_strncmp((ms()->cmd[i])->arg[0], "./", 2) == 0))
+				return (invoke_shell(i, path));
+		}
+	}
+	if (!path || execve(path, ms()->cmd[i]->arg, ms()->ms_env) == -1)
+		not_found(i);
+}
+
+void	close_heredoc(int i)
+{
+	int		fd;
+	t_redir	*r;
+
+	r = ms()->cmd[i]->fd_in;
+	while (r)
+	{
+		if (r->type == 2)
+		{
+			fd = ft_atoi(r->file);
+			if (fd >= 0)
+				close(fd);
+		}
+		r = r->next;
+	}
+}
